1ebff96aab6271d047a2674e9a928895215c0197
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-08-29 11:40:43 +0200

    jbake version update

--- Config.groovy
+++ Config.groovy
@@ -4,9 +4,10 @@ outputPath = 'build/docs'
 // This path is appended to the docDir property specified in gradle.properties
 // or in the command line, and therefore must be relative to it.
 
+inputPath = "./src/docs"
 
 inputFiles = [
-        [file: 'Part-3-StaticSiteGenerators.adoc',            formats: ['html','pdf']],
+        [file: 'Part-3-StaticSiteGenerators.adoc',            formats: ['html','pdf','docx','docbook']],
 ]
 
 taskInputsDirs = ["${inputPath}/images"]
